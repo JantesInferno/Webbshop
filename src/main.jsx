2e00105ffa672220df9b7a05c961a7a4446ce3a1
JantesInferno <123421286+JantesInferno@users.noreply.github.com>
2024-04-16 15:14:09 +0200

    router url

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import { CartContextProvider } from './contexts/CartContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={"/"}>
+    <BrowserRouter basename={"/Webbshop"}>
       <ThemeProvider theme={theme}>
         <AuthContextProvider>
           <CartContextProvider>
